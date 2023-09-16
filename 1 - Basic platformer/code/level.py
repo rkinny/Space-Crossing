b942538511d58f6e9df96da6751bb82210dbb4a7
carolineyting <145171082+carolineyting@users.noreply.github.com>
2023-09-16 01:08:57 -0400

    var

--- 1 - Basic platformer/code/level.py
+++ 1 - Basic platformer/code/level.py
@@ -8,30 +8,30 @@ class Level:
 		
 		# level setup
 		self.display_floor = floor 
-		self.setup_level(data)
-		self.world_shift = 0
-		self.current_x = 0
+		self.set_level(data)
+		self.shift = 0
+		self.currx = 0
 
-		self.player_on_ground = False
+		self.playerOnGround = False
 
-	def get_player_on_ground(self):
+	def getPlayerOnGround(self):
 		if self.player.sprite.on_ground:
-			self.player_on_ground = True
+			self.playerOnGround = True
 		else:
-			self.player_on_ground = False
+			self.playerOnGround = False
 
-	def setup_level(self,layout):
-		self.tiles = pygame.sprite.Group()
+	def set_level(self,lay):
+		self.t = pygame.sprite.Group()
 		self.player = pygame.sprite.GroupSingle()
 
-		for row_index,row in enumerate(layout):
-			for col_index,cell in enumerate(row):
-				x = col_index * tile_size
-				y = row_index * tile_size
+		for ri,row in enumerate(lay):
+			for ci,cell in enumerate(row):
+				x = ci * tile_size
+				y = ri * tile_size
 				
 				if cell == 'X':
 					tile = Tile((x,y),tile_size)
-					self.tiles.add(tile)
+					self.t.add(tile)
 				if cell == 'P':
 					player_sprite = Player((x,y),self.display_floor)
 					self.player.add(player_sprite)
@@ -42,40 +42,40 @@ class Level:
 		direction_x = player.direction.x
 
 		if player_x < screen_width / 4 and direction_x < 0:
-			self.world_shift = 8
+			self.shift = 8
 			player.speed = 0
 		elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
-			self.world_shift = -8
+			self.shift = -8
 			player.speed = 0
 		else:
-			self.world_shift = 0
+			self.shift = 0
 			player.speed = 8
 
 	def horizontal_movement_collision(self):
 		player = self.player.sprite
 		player.rect.x += player.direction.x * player.speed
 
-		for sprite in self.tiles.sprites():
+		for sprite in self.t.sprites():
 			if sprite.rect.colliderect(player.rect):
 				if player.direction.x < 0: 
 					player.rect.left = sprite.rect.right
 					player.on_left = True
-					self.current_x = player.rect.left
+					self.currx = player.rect.left
 				elif player.direction.x > 0:
 					player.rect.right = sprite.rect.left
 					player.on_right = True
-					self.current_x = player.rect.right
+					self.currx = player.rect.right
 
-		if player.on_left and (player.rect.left < self.current_x or player.direction.x >= 0):
+		if player.on_left and (player.rect.left < self.currx or player.direction.x >= 0):
 			player.on_left = False
-		if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
+		if player.on_right and (player.rect.right > self.currx or player.direction.x <= 0):
 			player.on_right = False
 
 	def vertical_movement_collision(self):
 		player = self.player.sprite
 		player.apply_gravity()
 
-		for sprite in self.tiles.sprites():
+		for sprite in self.t.sprites():
 			if sprite.rect.colliderect(player.rect):
 				if player.direction.y > 0: 
 					player.rect.bottom = sprite.rect.top
@@ -93,14 +93,14 @@ class Level:
 
 	def run(self):
 		# level tiles
-		self.tiles.update(self.world_shift)
-		self.tiles.draw(self.display_floor)
+		self.t.update(self.shift)
+		self.t.draw(self.display_floor)
 		self.scroll_x()
 
 
 		# player
 		self.player.update()
 		self.horizontal_movement_collision()
-		self.get_player_on_ground()
+		self.getPlayerOnGround()
 		self.vertical_movement_collision()
 		self.player.draw(self.display_floor)
