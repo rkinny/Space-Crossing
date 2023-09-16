1dbb927792d96c190c7522f41ab7126f24ca2b00
rkinny <118126084+rkinny@users.noreply.github.com>
2023-09-16 01:04:19 -0400

    tiles.py

--- 1 - Basic platformer/code/tiles.py
+++ 1 - Basic platformer/code/tiles.py
@@ -5,6 +5,8 @@ class Tile(pygame.sprite.Sprite):
 		super().__init__()
 		self.image = pygame.Surface((size,size))
 		self.image.fill('grey')
+		displayImage = pygame.image.load(r'../graphics/character/guy/background.JPG')
+		self.image.blit(displayImage,(0,0))
 		self.rect = self.image.get_rect(topleft = pos)
 
 	def update(self,x_shift):
