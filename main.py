0a835214078f7783ff9835d48eb07c6c39bb5b23
rkinny <118126084+rkinny@users.noreply.github.com>
2023-09-15 21:52:01 -0400

    added initial level

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pygame, sys
-pygame.init()
-screen_width = 300
-screen_height = 700
-#riya commit test
+level_map = [
+'                                                ',
+'                                                ',
+'                                                ',
+' XX                                             ',
+' XX P                                           ',
+' XXXX                                           ',
+' XXXX       XX                                  ',
+' XX    X  XXXX    XX  XX        XXXXX           ',
+'       X  XXXX    XX  XXX                       ',
+'    XXXX  XXXXXX  XX  XXXX  XXXX     XXXXXX     ',
+'XXXXXXXX  XXXXXX  XX  XXXX  XXXXXX              ']
+
+tile_size = 48 #64
+screen_width = 1500
+screen_height = len(level_map) * tile_size
